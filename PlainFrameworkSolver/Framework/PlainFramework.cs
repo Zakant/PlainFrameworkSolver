b6c9aa01509a75addc8326c5e9935b8825f3c804
Zakant <zakant@gmx.de>
2016-05-04 13:00:29 +0200

    Added functions to add and remove elements from and to the framework.

--- PlainFrameworkSolver/Framework/PlainFramework.cs
+++ PlainFrameworkSolver/Framework/PlainFramework.cs
@@ -31,6 +31,48 @@ namespace PlainFrameworkSolver.Framework
             Selected = element;
             if (Selected != null) Selected.IsSelected = true;
         }
+
+        public void RemoveElement(FrameworkElement element)
+        {
+            if(element is Bar)
+            {
+                var b = (Bar)element;
+                b.NodeA?.Detach(b);
+                b.NodeB?.Detach(b);
+                Bars.Remove(b);
+            }
+            else if(element is Node)
+            {
+                var n = (Node)element;
+                foreach (var f in n.Forces.ToList())
+                    RemoveElement(f);
+                Nodes.Remove(n);
+            }
+            else if(element is ExternalForce)
+            {
+                var e = (ExternalForce)element;
+                e.Target.Detach(e);
+                ExternalForces.Remove(e);
+            }
+        }
+
+        public void AddElement(FrameworkElement element)
+        {
+            if (element is Bar)
+            {
+                var b = (Bar)element;
+                b.NodeA?.Attach(b);
+                b.NodeB?.Attach(b);
+                Bars.Add(b);
+            }
+            else if (element is Node) Nodes.Add((Node)element);
+            else if (element is ExternalForce)
+            {
+                var f = (ExternalForce)element;
+                f.Target?.Attach(f);
+                ExternalForces.Add(f);
+            }
+        }
     }
 }
 
