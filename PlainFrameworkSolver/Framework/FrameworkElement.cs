6dc79d44568c6fa73c28e93088a2173764027c2c
Zakant <zakant@gmx.de>
2016-05-05 12:42:28 +0200

    Added INotifyPropertyChanged.

--- PlainFrameworkSolver/Framework/FrameworkElement.cs
+++ PlainFrameworkSolver/Framework/FrameworkElement.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace PlainFrameworkSolver.Framework
 {
-    public abstract class FrameworkElement : IDrawable
+    public abstract class FrameworkElement : IDrawable, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Name { get; set; }
 
         public bool IsSelected { get; set; }
 
         public abstract void Draw(Graphics g, Rectangle boundary);
+
+        protected void RaisePropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
