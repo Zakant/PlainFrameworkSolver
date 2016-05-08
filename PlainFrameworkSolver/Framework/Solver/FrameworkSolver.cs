bd111f6f5fbbf2a866d4c67e245f94a1727cb001
Zakant <zakant@gmx.de>
2016-05-08 15:25:49 +0200

    Added Solver class.

---
+++ PlainFrameworkSolver/Framework/Solver/FrameworkSolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainFrameworkSolver.Framework.Solver
+{
+    public class FrameworkSolver
+    {
+        public PlainFramework Framework { get; protected set; }
+
+        public FrameworkSolver(PlainFramework framework)
+        {
+            Framework = framework;
+        }
+
+        public void CreateMatrix()
+        {
+        }
+
+        public Dictionary<string, double> Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
