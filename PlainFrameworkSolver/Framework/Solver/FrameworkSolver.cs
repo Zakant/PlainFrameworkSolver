9ff484413ca66216de553e73a6a5ae438d36edc1
Sönke Matthiesen <soenke02@matthiesen5mal.de>
2016-06-14 13:41:47 +0200

    Fix #20.

--- PlainFrameworkSolver/Framework/Solver/FrameworkSolver.cs
+++ PlainFrameworkSolver/Framework/Solver/FrameworkSolver.cs
@@ -42,7 +42,7 @@ namespace PlainFrameworkSolver.Framework.Solver
             var list = new List<ResultEntry>();
             var solution = system.Solve();
             for (int i = 0; i < solution.Length; i++)
-                list.Add(new ResultEntry(Index[i], solution[i]));
+                list.Add(new ResultEntry(Index[i], Math.Round(solution[i], 3)));
             return list;
         }
     }
